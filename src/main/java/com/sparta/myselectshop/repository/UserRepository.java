0e5e214218db5796a5b27c6a70d53fbeab258d8a
addydfdf <addydfdf>
2022-12-12 02:38:47 +0900

    심화주차 커밋하기

--- src/main/java/com/sparta/myselectshop/repository/UserRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/UserRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //ㅋ카카오 아이디가 있는지 없는지 확인
     Optional<User> findByUsername(String username);
+    Optional<User> findByKakaoId(Long id);
+    Optional<User> findByEmail(String email);
+
+    //이메일 중복 되었는지 혹인
 }
--- src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
+++ src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
@@ -2,6 +2,7 @@ package com.sparta.myselectshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class MyselectshopApplicationTests {
